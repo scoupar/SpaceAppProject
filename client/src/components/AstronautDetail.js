9c5ea0592a1be1d357729462faba3715c96d9b1e
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-02-28 16:27:19 +0000

    fixed server issue

---
+++ client/src/components/AstronautDetail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AstronautDetail = () => {
+    return(
+        <h1>Hello Astronaut details!</h1>
+    )
+}
+
+export default AstronautDetail;
--- server/server.js
+++ server/server.js
@@ -19,6 +19,6 @@ MongoClient.connect('mongodb://localhost:27017')
     })
     .catch(console.error);
 
-app.listen(5001, function (){
+app.listen(5000, function (){
     console.log(`listening on port ${this.address().port}`);
 });
