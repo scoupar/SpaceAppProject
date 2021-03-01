e683e4079e6abeccdb3080e3a74158de1406a41b
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-01 22:26:12 +0000

    Added custom icon for map

--- client/src/components/LaunchMap.js
+++ client/src/components/LaunchMap.js
@@ -4,53 +4,22 @@ import L from 'leaflet';
 import './LaunchMap.css';
 import { render } from 'react-dom';
 import "leaflet/dist/leaflet.css"
+import Rocket from './Rocket.png';
+import {Icon} from 'leaflet'
 
 const LaunchMap = () => {
 
-    // class LaunchMap extends Component {
-
-    //     componentDidMount(){
-    //         const mymap = L.map('mapid').setView([51.505, -0.09], 13);
-    //     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    //         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    //         maxZoom: 18,
-    //         id: 'mapbox/streets-v11',
-    //         tileSize: 512,
-    //         zoomOffset: -1,
-    //         accessToken: 'pk.eyJ1IjoiYWMyMzIiLCJhIjoiY2tscWYzZmdkMWJ5ZjJ3cXI1bXF0cHJzaCJ9.qm_MAS4aD7mhOzDVwP32Ig'
-    //     }).addTo(mymap);
-    //     }
-    // }
-
-    //     render(); {
-    //         return(
-    //             <div id="mapid"/>
-    //         );
-    //     }
-
-
-    // const container = document.getElementById('mapid')
-    // if(container) {
-    //     const mymap = L.map('mapid').setView([51.505, -0.09], 13);
-    //     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    //         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    //         maxZoom: 18,
-    //         id: 'mapbox/streets-v11',
-    //         tileSize: 512,
-    //         zoomOffset: -1,
-    //         accessToken: 'pk.eyJ1IjoiYWMyMzIiLCJhIjoiY2tscWYzZmdkMWJ5ZjJ3cXI1bXF0cHJzaCJ9.qm_MAS4aD7mhOzDVwP32Ig'
-    //     }).addTo(mymap);
-    // }
+    
     return(
         <div id="mapid">
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer center={[0, 0]} zoom={1} scrollWheelZoom={false}>
                 <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
+                <Marker position={[-39.258663, 177.866849]} icon={new Icon({iconUrl: Rocket, iconSize: [25,41], iconAnchor: [12, 41]})}>
                 <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
+                Onenui Station, Mahia Peninsula, New Zealand
                 </Popup>
                 </Marker>
             </MapContainer>
