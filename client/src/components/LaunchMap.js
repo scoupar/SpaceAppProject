a8ee982009724daf3e8a7b2c8d78c9c52575b187
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-01 22:45:16 +0000

    added further map markers

--- client/src/components/LaunchMap.js
+++ client/src/components/LaunchMap.js
@@ -32,6 +32,31 @@ const LaunchMap = () => {
                 Baikonur Cosmodrome, Republic of Kazakhstan
                 </Popup>
                 </Marker>
+                <Marker position={[13.733056, 80.204722]} icon={new Icon({iconUrl: Rocket, iconSize: [25,41], iconAnchor: [12, 41]})}>
+                <Popup>
+                Sriharikota, Republic of India
+                </Popup>
+                </Marker>
+                <Marker position={[34.75821, -120.516395]} icon={new Icon({iconUrl: Rocket, iconSize: [25,41], iconAnchor: [12, 41]})}>
+                <Popup>
+                Vandenberg AFB, CA, USA
+                </Popup>
+                </Marker>
+                <Marker position={[28.572872, -80.648981]} icon={new Icon({iconUrl: Rocket, iconSize: [25,41], iconAnchor: [12, 41]})}>
+                <Popup>
+                Kennedy Space Center, FL, USA
+                </Popup>
+                </Marker>
+                <Marker position={[28.392218, -80.607713]} icon={new Icon({iconUrl: Rocket, iconSize: [25,41], iconAnchor: [12, 41]})}>
+                <Popup>
+                Cape Canaveral, FL, USA
+                </Popup>
+                </Marker>
+                <Marker position={[25.997316, -97.157299]} icon={new Icon({iconUrl: Rocket, iconSize: [25,41], iconAnchor: [12, 41]})}>
+                <Popup>
+                SpaceX Space Launch Facility, TX, USA
+                </Popup>
+                </Marker>
             </MapContainer>
         </div>
     )
