64bed28bf4d32c4ad5ccd44ab21ec78a48c65bb7
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-03-02 21:54:16 +0000

    fixed pie chart

--- client/src/components/AstronautChart.js
+++ client/src/components/AstronautChart.js
@@ -5,8 +5,6 @@ import HighchartsReact from 'highcharts-react-official';
 
 const AstronautChart = ({allAstronauts}) => {
 
-
-
     const options = {
         chart:{
             type: 'pie',
@@ -22,14 +20,83 @@ const AstronautChart = ({allAstronauts}) => {
         series:[
             {   
                 data:[
-                    ['American', 120],
-                    ['Russian', 250],
-                    ['Chinese', 10],
-                    ['Earthling', 15]
+                    ['American', 52],
+                    ['Russian', 13],
+                    ['Chinese', 11],
+                    ['Earthling', 5],
+                    ['Japanese', 7],
+                    ['French', 2],
+                    ['Canadian', 2],
+                    ['British', 1],
+                    ['German', 1],
+                    ['Brazilian', 1],
+                    ['Emirati', 1],
+                    ['Swedish', 1],
+                    ['Danish', 1],
+                    ['Malaysian', 1],
+                    ['Italian', 1]
+
                 ]
             }
         ]
     }
+    const filteredAstronauts = allAstronauts.map((astronaut) => {
+        return astronaut.nationality
+    })
+    const uniqueNationalities = [...new Set(filteredAstronauts)]
+    
+    
+    
+    
+       
+    const newFilteredAstronauts = allAstronauts.filter((astronaut) => {
+    return astronaut.nationality
+    })
+    const nationalities = newFilteredAstronauts.map((astronaut, index) => {
+    return astronaut.nationality
+    })
+    
+    
+    const arr = nationalities
+    const calculateCount = (arr, query) => {
+       let count = 0;
+       for(let i = 0; i < arr.length; i++){
+          if(arr[i] === query){
+             count++;
+             continue;
+       };
+       if(Array.isArray(arr[i])){
+          count += calculateCount(arr[i], query);
+       }
+    };
+    return count;
+    };
+    
+    const list = uniqueNationalities.map((nationality, index) => {
+        for (nationality of uniqueNationalities){
+            calculateCount(arr,nationality)
+        }
+    })
+
+
+    const newFinalList = [... new Set(list)] 
+    console.log(newFinalList)
+    
+    
+    const space = list.forEach((nationality) => {
+        calculateCount(arr, nationality);
+        console.log(nationality)
+
+    })
+    
+    console.log(filteredAstronauts);
+    
+    
+    console.log(calculateCount(arr, "Swedish"));
+    
+    const finalList = [... new Set(space)];
+    console.log(finalList);
+
     return(
         <div className= "chart">
             
@@ -37,63 +104,12 @@ const AstronautChart = ({allAstronauts}) => {
           
         </div>
     )
+    
 }
 
+
+    
+
+
+
 export default AstronautChart;
-
-
-    
-    // const filteredAstronauts = allAstronauts.map((astronaut) => {
-    //         return astronaut.nationality
-    // })
-    // const uniqueNationalities = [...new Set(filteredAstronauts)]
-
-    // // const list = uniqueNationalities.map((nationality, index) => {
-    // //     return (
-    // //         <li className = "nationality-list">{nationality}</li>
-    // //      )
-    // // })
-           
-    // const newFilteredAstronauts = allAstronauts.filter((astronaut) => {
-    //     return astronaut.nationality
-    // })
-    // const nationalities = newFilteredAstronauts.map((astronaut, index) => {
-    //     return astronaut.nationality
-    // })
-
-
-    // const arr = nationalities
-    //     const calculateCount = (arr, query) => {
-    //        let count = 0;
-    //        for(let i = 0; i < arr.length; i++){
-    //           if(arr[i] === query){
-    //              count++;
-    //              continue;
-    //        };
-    //        if(Array.isArray(arr[i])){
-    //           count += calculateCount(arr[i], query);
-    //        }
-    //     };
-    //     return count;
-    //  };
-    
-    // const list = uniqueNationalities.map((nationality, index) => {
-    //     for (nationality of uniqueNationalities){
-    //         calculateCount(arr,nationality)
-    //     }
-         
-    // })
-    // const newFinalList = [... new Set(list)] 
-    //      console.log(newFinalList)
-
-    
-    //  const space = list.forEach((nationality) => {
-    //     calculateCount(arr, nationality);
-    //     // console.log(nationality)
-
-        
-    
-    //  console.log(calculateCount(arr, "Earthling"));
-
-    //  const finalList = [... new Set(space)];
-    //  console.log(finalList);
