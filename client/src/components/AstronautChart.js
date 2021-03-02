7236c571088454f8b52ccec45919eeb371a417bd
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-03-02 21:59:17 +0000

    styling

--- client/src/components/AstronautChart.js
+++ client/src/components/AstronautChart.js
@@ -40,62 +40,7 @@ const AstronautChart = ({allAstronauts}) => {
             }
         ]
     }
-    const filteredAstronauts = allAstronauts.map((astronaut) => {
-        return astronaut.nationality
-    })
-    const uniqueNationalities = [...new Set(filteredAstronauts)]
-    
-    
-    
-    
-       
-    const newFilteredAstronauts = allAstronauts.filter((astronaut) => {
-    return astronaut.nationality
-    })
-    const nationalities = newFilteredAstronauts.map((astronaut, index) => {
-    return astronaut.nationality
-    })
-    
-    
-    const arr = nationalities
-    const calculateCount = (arr, query) => {
-       let count = 0;
-       for(let i = 0; i < arr.length; i++){
-          if(arr[i] === query){
-             count++;
-             continue;
-       };
-       if(Array.isArray(arr[i])){
-          count += calculateCount(arr[i], query);
-       }
-    };
-    return count;
-    };
-    
-    const list = uniqueNationalities.map((nationality, index) => {
-        for (nationality of uniqueNationalities){
-            calculateCount(arr,nationality)
-        }
-    })
-
-
-    const newFinalList = [... new Set(list)] 
-    console.log(newFinalList)
-    
-    
-    const space = list.forEach((nationality) => {
-        calculateCount(arr, nationality);
-        console.log(nationality)
-
-    })
-    
-    console.log(filteredAstronauts);
-    
-    
-    console.log(calculateCount(arr, "Swedish"));
-    
-    const finalList = [... new Set(space)];
-    console.log(finalList);
+   
 
     return(
         <div className= "chart">
